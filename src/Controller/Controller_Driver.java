e492753f87901ed2aff880234479ef48abd46a6e
DuodecimAD <tonyxii@gmail.com>
2023-05-02 19:08:52 +0200

    Stats view done, needs to do travel to fix stats outofbound problem

--- src/Controller/Controller_Driver.java
+++ src/Controller/Controller_Driver.java
@@ -1,29 +1,12 @@
-/*
- * 
- */
 package Controller;
 
 import Model.Driver;
-
 import java.util.ArrayList;
-import java.util.Scanner;
 
-/**
- * The Class Controller_Driver.
- */
 public class Controller_Driver {
-
-    /** The List drivers. */
     public final ArrayList<Driver> list_drivers = new ArrayList<>();
-
     private Driver current_driver;
-    //private View_Driver view_driver;
 
-
-
-    /**
-     * Instantiates a new controller driver.
-     */
     public Controller_Driver() {
 
         list_drivers.add(new Driver(0,"JP", "Belmondo", 15, "cemetery"));
@@ -33,182 +16,19 @@ public class Controller_Driver {
         list_drivers.add(new Driver(4,"Sean", "Connery", 35, "sadly ..."));
 
         getLastDriver();
-
-
     }
 
-    /**
-     * Menu.
-     */
-    public void menu(){
-
-        int choix;
-        Scanner scanner = new Scanner(System.in);
-
-        do
-        {
-            if (list_drivers.size() > 0)
-            {
-                do
-                {
-                    System.out.println("\u001B[95m");
-                    System.out.println(current_driver);
-                    System.out.println("\u001B[36m");
-                    System.out.println("*******************");
-                    System.out.println("*** Menu Driver ***");
-                    System.out.println("*******************");
-                    System.out.print("\u001B[0m");
-                    System.out.println("1. New");
-                    System.out.println("2. Update");
-                    System.out.println("3. Delete");
-                    System.out.println("4. Previous");
-                    System.out.println("5. Next");
-                    System.out.println("6. First");
-                    System.out.println("7. Last");
-                    System.out.println("8. Select Driver");
-                    System.out.println("9. Back to Main menu..");
-                    System.out.println("Your choice ? : ");
-                    choix = scanner.nextInt();
-                    scanner.nextLine();
-
-                    switch (choix) {
-                        case 1 -> Driver_Create();
-                        case 2 -> Driver_Update();
-                        case 3 -> Driver_Delete();
-                        case 4 -> goPreviousDriver();
-                        case 5 -> goNextDriver();
-                        case 6 -> getFirstDriver();
-                        case 7 -> getLastDriver();
-                        case 8 -> Driver_Select();
-                        case 9 -> System.out.println("Back to main menu");
-                        default -> System.out.println("\u001B[31m" + "Error, please choose a number from the menu" + "\u001B[0m");
-                    }
-                }while(choix<0||choix>9);
-            }
-            else
-            {
-                do
-                {
-                    System.out.println("\u001B[95m");
-                    System.out.println("No Driver in List");
-                    System.out.println("\u001B[36m");
-                    System.out.println("*******************");
-                    System.out.println("*** Driver Menu ***");
-                    System.out.println("*******************");
-                    System.out.print("\u001B[0m");
-                    System.out.println("1. New");
-                    System.out.println("9. Back to Main menu..");
-                    System.out.println("Your choice ? : ");
-                    choix=scanner.nextInt();
-
-                    switch (choix) {
-                        case 1 -> Driver_Create();
-                        case 9 -> System.out.println("Back to main menu..");
-                        default -> System.out.println("\u001B[31m" + "Error, please choose a number from the menu" + "\u001B[0m");
-                    }
-                }while(list_drivers.size()==0);
-            }
-        }while(choix!=9);
-    }
-
-    /**
-     * Driver create.
-     */
-    public void Driver_Create(){
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Please enter the Driver's First name : ");
-        String first_name = scanner.nextLine();
-        System.out.println("Please enter the Driver's Last Name : ");
-        String last_name = scanner.nextLine();
-        System.out.println("Please enter the Driver's age : ");
-        int age = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Please enter the Driver's address : ");
-        String address = scanner.nextLine();
-        int key = list_drivers.size();
-
-        list_drivers.add(new Driver(key,first_name,last_name,age,address));
-        getLastDriver();
-    }
-
-    /**
-     * Driver update.
-     */
-    public void Driver_Update(){
-
-        Scanner scanner = new Scanner(System.in);
-        int choix;
-        do
-        {
-            System.out.println("\u001B[94m");
-            System.out.println(current_driver);
-            System.out.println("\u001B[36m");
-            System.out.println("****************************");
-            System.out.println("What do you want to Update ?");
-            System.out.println("****************************");
-            System.out.print("\u001B[0m");
-            System.out.println("1.Driver's first name");
-            System.out.println("2.Driver's last name");
-            System.out.println("3.Driver's age");
-            System.out.println("4.Driver's address");
-            System.out.println("9. Back to Driver menu..");
-            System.out.println("Your choice ? : ");
-            choix = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (choix) {
-                case 1 -> {
-                    System.out.println("What's the new Driver's first name : ");
-                    String first_name = scanner.nextLine();
-                    current_driver.setDriver_first_name(first_name);
-                }
-                case 2 -> {
-                    System.out.println("What's the new Driver's last name : ");
-                    String last_name = scanner.nextLine();
-                    current_driver.setDriver_last_name(last_name);
-                }
-                case 3 -> {
-                    System.out.println("What's the new Driver's age : ");
-                    int age = scanner.nextInt();
-                    scanner.nextLine();
-                    current_driver.setDriver_age(age);
-                }
-                case 4 -> {
-                    System.out.println("What's the new Driver's address : ");
-                    String address = scanner.nextLine();
-                    current_driver.setDriver_address(address);
-                }
-                case 9 -> System.out.println("Back to Driver menu..");
-                default -> System.out.println("\u001B[31m" + "Error, please choose a number from the menu" + "\u001B[0m");
-            }
-        }while(choix !=9);
-
-    }
-
-    /**
-     * Driver checklist.
-     */
     public void Driver_Delete(){
-
         list_drivers.remove(current_driver);
 
         for (int i = 0; i < list_drivers.size(); i++) {
             list_drivers.get(i).setDriver_id(i);
         }
-
         getLastDriver();
-
     }
-
     public int getCurrentIndex(){
         return list_drivers.indexOf(current_driver);
     }
-
-    /**
-     * Driver previous.
-     */
     public void goPreviousDriver(){
         int next_index = getCurrentIndex()-1;
         System.out.println(next_index);
@@ -221,11 +41,6 @@ public class Controller_Driver {
     public String goPreviousDriverError(){
         return "You are on the first Driver.";
     }
-
-
-    /**
-     * Driver next.
-     */
     public void goNextDriver(){
         int next_index = getCurrentIndex()+1;
 
@@ -234,83 +49,26 @@ public class Controller_Driver {
         }else{
             System.out.println(goNextDriverError());
         }
-
     }
     public String goNextDriverError(){
         return "You are on the last Driver.";
     }
-
     public void getFirstDriver() {
         current_driver = list_drivers.get(0);
     }
-
     public void getLastDriver() {
        current_driver = list_drivers.get(list_drivers.size()-1);
-
     }
-
     public Driver getCurrentDriver() {
         return current_driver;
     }
-
     public void setCurrent_driver(Driver current_driver) {
         this.current_driver = current_driver;
     }
-
-    /**
-     * Driver select.
-     */
-    public void Driver_Select(){
-
-        int choix;
-        Scanner scanner = new Scanner(System.in);
-
-        do {
-            System.out.println("\u001B[36m");
-            System.out.println("*************");
-            System.out.println("Select Driver");
-            System.out.println("*************");
-            System.out.print("\u001B[0m");
-            for (Driver driver : list_drivers) {
-                System.out.println("Driver "+
-                    list_drivers.indexOf(driver)+" : "+
-                    driver.getDriver_first_name() +" "+
-                    driver.getDriver_last_name()
-                );
-            }
-            System.out.println("Please enter your choice : ");
-            choix = scanner.nextInt();
-            scanner.nextLine();
-            if(choix < 0 || choix >= list_drivers.size()){
-                System.out.print("\u001B[31m");
-                System.out.println("Error, please choose a number from the menu");
-                System.out.print("\u001B[0m");
-            }
-        }while(choix < 0 || choix >= list_drivers.size());
-
-        current_driver = list_drivers.get(choix);
-    }
-
-    /**
-     * Driver display name.
-     *
-     * @param i the i
-     * @return the string
-     */
     public String Driver_display_name(int i){
-
         return list_drivers.get(i).getDriver_first_name()+" "+list_drivers.get(i).getDriver_last_name();
     }
-
-    /**
-     * Driver length.
-     *
-     * @return the int
-     */
     public int Driver_length(){
-
         return list_drivers.size();
     }
-
-
 }
--- src/View/View_Main.java
+++ src/View/View_Main.java
@@ -1,6 +1,8 @@
 package View;
 
 import Controller.Controller_Driver;
+import Controller.Controller_Travel;
+import Controller.Controller_Truck;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,10 +11,8 @@ import java.awt.event.ActionListener;
 
 public class View_Main extends JFrame{
     Controller_Driver c_driver = new Controller_Driver();
-    private View_Driver v_driver;
-    private View_Truck v_truck;
-    private View_Travel v_travel;
-    private View_Stats v_stats;
+    Controller_Travel c_travel = new Controller_Travel();
+    Controller_Truck c_truck = new Controller_Truck();
 
     private JButton driversButton;
     private JButton trucksButton;
@@ -72,35 +72,29 @@ public class View_Main extends JFrame{
     }
 
     public void show_View_Driver() {
-        v_driver = new View_Driver(View_Main.this, c_driver);
+        View_Driver v_driver = new View_Driver(View_Main.this, c_driver);
         v_driver.add(v_driver.getDriverPanel());
         setContentPane(v_driver);
-        //revalidate();
-        //repaint();
-        // pack, center, and show your new view here
-        //pack();
-        //setVisible(true);
     }
 
     public void show_View_Truck() {
-        v_truck = new View_Truck();
+        View_Truck v_truck = new View_Truck();
         setContentPane(v_truck);
         // pack, center, and show your new view here
         pack();
     }
 
     public void show_View_Travel() {
-        v_travel = new View_Travel();
+        View_Travel v_travel = new View_Travel();
         setContentPane(v_travel);
         // pack, center, and show your new view here
         pack();
     }
 
     public void show_View_Stats() {
-        v_stats = new View_Stats();
+        View_Stats v_stats = new View_Stats(View_Main.this, c_travel, c_driver, c_truck);
+        v_stats.add(v_stats.getDriverPanel());
         setContentPane(v_stats);
-        // pack, center, and show your new view here
-        pack();
     }
 
     public JPanel getMainPanel() {
