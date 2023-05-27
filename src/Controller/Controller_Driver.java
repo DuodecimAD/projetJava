2e7657ac0c4b3c5e6833fe294d8f137a0bfd651e
DuodecimAD <tonyxii@gmail.com>
2023-05-27 16:50:09 +0200

    switching from arrayList or hashMap

--- src/Controller/Controller_Driver.java
+++ src/Controller/Controller_Driver.java
@@ -1,29 +1,41 @@
 package Controller;
 
 import Model.Driver;
+
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Controller_Driver {
-    public final ArrayList<Driver> list_drivers = new ArrayList<>();
+    //public final ArrayList<Driver> map_drivers = new ArrayList<>();
+    public final LinkedHashMap<Integer, Driver> map_drivers = new LinkedHashMap<>();
     private Driver current_driver;
 
     public Controller_Driver() {
 
-        list_drivers.add(new Driver(0,"JP", "Belmondo", 15, "cemetery"));
-        list_drivers.add(new Driver(1,"Louis", "de Funes", 20, "cemetery too"));
-        list_drivers.add(new Driver(2,"Jean", "Gabin", 25, "same"));
-        list_drivers.add(new Driver(3,"Chuck", "Noris", 30, "i don't know, if not, soon"));
-        list_drivers.add(new Driver(4,"Sean", "Connery", 35, "sadly ..."));
+//        list_drivers.add(new Driver(0,"JP", "Belmondo", 15, "cemetery"));
+//        list_drivers.add(new Driver(1,"Louis", "de Funes", 20, "cemetery too"));
+//        list_drivers.add(new Driver(2,"Jean", "Gabin", 25, "same"));
+//        list_drivers.add(new Driver(3,"Chuck", "Noris", 30, "i don't know, if not, soon"));
+//        list_drivers.add(new Driver(4,"Sean", "Connery", 35, "sadly ..."));
+
+        map_drivers.put(0, new Driver(0, "JP", "Belmondo", 15, "cemetery"));
+        map_drivers.put(1, new Driver(1, "Louis", "de Funes", 20, "cemetery too"));
+        map_drivers.put(2, new Driver(2, "Jean", "Gabin", 25, "same"));
+        map_drivers.put(3, new Driver(3, "Chuck", "Noris", 30, "i don't know, if not, soon"));
+        map_drivers.put(4, new Driver(4, "Sean", "Connery", 35, "sadly ..."));
 
         getLastDriver();
     }
 
     public void Driver_Delete(){
-        list_drivers.remove(current_driver);
+        map_drivers.remove(current_driver.getDriver_id());
 
-        for (int i = 0; i < list_drivers.size(); i++) {
-            list_drivers.get(i).setDriver_id(i);
-        }
+
+
+//        for (int i = 0; i < list_drivers.size(); i++) {
+//            list_drivers.get(i).setDriver_id(i);
+//        }
 
         //System.out.println(list_drivers.get(0).getDriver_id());
         getLastDriver();
@@ -31,38 +43,41 @@ public class Controller_Driver {
     }
 
 
-    public int getDriverCurrentIndex(){
-        return list_drivers.indexOf(current_driver);
-    }
     public void goPreviousDriver(){
-        int next_index = getDriverCurrentIndex()-1;
-        System.out.println(next_index);
-        if(next_index >= 0){
-            current_driver = list_drivers.get(next_index);
-        }else{
-            System.out.println(goPreviousDriverError());
+        for (int i = 1; i < map_drivers.size(); i++) {
+            if(map_drivers.containsKey(current_driver.getDriver_id()-i)) {
+                current_driver = map_drivers.get(current_driver.getDriver_id()-i);
+            }
         }
     }
     public String goPreviousDriverError(){
         return "You are on the first Driver.";
     }
     public void goNextDriver(){
-        int next_index = getDriverCurrentIndex()+1;
-
-        if(next_index < list_drivers.size()){
-            current_driver = list_drivers.get(next_index);
-        }else{
-            System.out.println(goNextDriverError());
+        for (int i = 1; i < map_drivers.size(); i++) {
+            if(map_drivers.containsKey(current_driver.getDriver_id()+i)) {
+                current_driver = map_drivers.get(current_driver.getDriver_id()+i);
+            }
         }
     }
     public String goNextDriverError(){
         return "You are on the last Driver.";
     }
     public void getFirstDriver() {
-        current_driver = list_drivers.get(0);
+        current_driver = map_drivers.get(0);
     }
     public void getLastDriver() {
-       current_driver = list_drivers.get(list_drivers.size()-1);
+
+        int maxKey = -1;
+
+        for (Map.Entry<Integer, Driver> driver : map_drivers.entrySet()) {
+            int key = driver.getKey();
+            if (key > maxKey) {
+                maxKey = key;
+            }
+        }
+        current_driver = map_drivers.get(maxKey);
+
     }
     public Driver getCurrentDriver() {
         return current_driver;
@@ -71,9 +86,9 @@ public class Controller_Driver {
         this.current_driver = current_driver;
     }
     public String Driver_display_name(int i){
-        return list_drivers.get(i).getDriver_first_name()+" "+list_drivers.get(i).getDriver_last_name();
+        return map_drivers.get(i).getDriver_first_name()+" "+ map_drivers.get(i).getDriver_last_name();
     }
     public int Driver_length(){
-        return list_drivers.size();
+        return map_drivers.size();
     }
 }
